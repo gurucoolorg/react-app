db2f418063c5ee107a874bfa4ddcafb0d3cd12c7
akash shukla <shuklaji.2227@gmail.com>
2020-07-17 21:20:53 +0530

    Updates

---
+++ precache-manifest.7a24dc3d8a8651e1a6f2141aae262b90.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8982c2cbd3fe8252fa66933404105a1c",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "f6a7c1edf9037f679f00",
+    "url": "/react-app/static/css/main.9186333e.chunk.css"
+  },
+  {
+    "revision": "0b7e2c0867324738495c",
+    "url": "/react-app/static/js/2.2b8783be.chunk.js"
+  },
+  {
+    "revision": "5235a5429e626c15d61916dfe11adc52",
+    "url": "/react-app/static/js/2.2b8783be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6a7c1edf9037f679f00",
+    "url": "/react-app/static/js/main.69178546.chunk.js"
+  },
+  {
+    "revision": "bf2e83a1fb7b8cfd1d15",
+    "url": "/react-app/static/js/runtime-main.dd4644f4.js"
+  }
+]);
